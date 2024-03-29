c37170f8016633b8b632f0677d863c34a44aa079
wliuye <161380356+wliuye@users.noreply.github.com>
2024-03-29 04:24:26 -0700

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function refreshWeather(response) {
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
-    
+
 
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
@@ -17,6 +17,9 @@ function refreshWeather(response) {
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+    
+    getForecast(response.data.city);
+
 }
 
 function formatDate(date) {
@@ -40,12 +43,6 @@ function formatDate(date) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function searchCity(city) {
-    let apiKey = "a4644bb8o376d1a3f49d2b2bf3fe0tcf";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(refreshWeather);
-}
-
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
@@ -53,7 +50,51 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function searchCity(city) {
+  let apiKey = "a4644bb8o376d1a3f49d2b2bf3fe0tcf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function getForecast (city){
+    let apiKey = "a4644bb8o376d1a3f49d2b2bf3fe0tcf";
+    let apiUrl =
+      `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+    console.log(response.data);
+
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat" ];
+    let forecastHtml = "";
+
+    days.forEach(function (day) {
+        forecastHtml =
+            forecastHtml +
+             `
+            <div class="weather-forecast-day">
+                <div class="weather-forecast-date">${day}</div>
+                    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png" alt="" width="36"/>
+                    <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temperature-max"></span>
+                            18° 
+                        <span class="weather-forecast-temperature-min"></span>
+                            20°
+                </div>
+            </div>
+        `;
+    });
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
+let searchElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
 searchCity("Quito");
+getForecast("city")
